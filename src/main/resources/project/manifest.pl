575ad7ff3f4a86925f521b6a51051b20717744ca
Valentyn Solomko <Valentyn.Solomko@portaone.com>
2019-01-09 12:33:40 +0200

    Adapt for building with Gradle

--- src/main/resources/project/manifest.pl
+++
@@ -1,5 +0,0 @@
-@files = (
-    ['//property[propertyName="silkDriver"]/value', 'silkDriver.pl'],
-    ['//procedure[procedureName="runSilk"]/propertySheet/property[propertyName="ec_parameterForm"]/value', 'runSilkForm.xml'],
-    ['//property[propertyName="ec_setup"]/value', 'ec_setup.pl'],
-);
